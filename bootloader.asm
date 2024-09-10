2edc4940243467499522e120f36ad3f12a352980
Sergio <sergiorc2000@hotmail.es>
2024-09-10 01:09:36 -0600

    Juego con reconocimiento de teclas y nombre

--- bootloader.asm
+++ bootloader.asm
@@ -8,20 +8,44 @@ start:
     
     ; Mostrar mensaje de bienvenida
     mov si, welcome_msg ; Dirección del mensaje
+    
     call print_string   ; Llama a la función para imprimir la cadena
 
     ; Esperar confirmación para iniciar
 wait_for_key:
     mov ah, 0x00        ; Función 0 de BIOS para esperar una tecla
     int 0x16            ; Interrupción de BIOS 16h (Teclado)
-    cmp al, 'Y'         ; Compara la tecla presionada con 'Y'
+    cmp al, 'y'         ; Compara la tecla presionada con 'Y'
     je start_game       ; Si es 'Y', inicia el juego
     jmp wait_for_key    ; Si no es 'Y', espera otra vez
 
+   
 start_game:
-    call clear_screen   ; Limpia la pantalla antes de iniciar el juego
-    mov si, game_msg
+    
+    ; Limpia la pantalla
+    mov ax, 0x0003      ; 03h es la función de BIOS para limpiar la pantalla
+    int 0x10            ; Llama a la interrupción de BIOS 10h (Video)
+    
+
+
+    call randomize_positions
+    
+
+    call print_names
+    mov si, msg
     call print_string
+    mov ah, 0x00           ; Llamada para esperar una tecla
+    int 0x16
+    
+    ; Procesar teclas de flechas
+    jmp process_arrows
+    
+
+
+    ;cmp al, 0             ; Comprobar si es tecla especial (flecha)
+    ;je process_arrows
+    ;jmp start_game
+
     
 hang:
     jmp hang            ; Bucle infinito para detener el bootloader después de iniciar el juego
@@ -44,10 +68,153 @@ clear_screen:
     int 0x10            ; Llama a la interrupción de BIOS 10h (Video)
     ret
 
+print_names:
+    name db '+   +  +++  +   +  +++  +  +', 0x0D,0x0A
+         db '+ +    +    +   +   +   ++ +', 0x0D,0x0A
+         db '++     ++   +   +   +   + ++', 0x0D,0x0A
+         db '+ +    +    +   +   +   + ++', 0x0D,0x0A
+         db '+   +  +++    +    +++  +  +', 0x0D,0x0A,0
+
+    mov si, name
+    call print_at_position
+
+    ret
+
+process_arrows:
+    mov ah, 00h           ; Leer el código extendido de la tecla
+    int 16h
+
+    cmp al, 'a'           ; Flecha izquierda
+    je move_left
+    
+    cmp al, 'd'           ; Flecha derecha
+    je move_right
+
+    cmp al, 'w'           ; Flecha arriba
+    je print_names
+
+    cmp al, 's'           ; Flecha abajo
+    je move_down
+
+    cmp al, 'p'           ; Si la tecla es 'p', salir
+    je exit
+
+    jmp process_arrows
+
+move_left:
+
+    left_name db '+++++', 0x0D,0x0A
+              db '  ++ ', 0x0D,0x0A
+              db ' +   ', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '++++ ', 0x0D,0x0A
+              db '    +', 0x0D,0x0A
+              db '++++ ', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '+ + +', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db ' + + ', 0x0D,0x0A
+              db '  +  ', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A,0    
+
+    mov si, left_name
+    call print_at_position
+    jmp process_arrows
+
+move_right:
+        right_name db '+++++', 0x0D,0x0A
+              db '  +  ', 0x0D,0x0A
+              db ' + + ', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A
+              db '+ + +', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db ' ++++', 0x0D,0x0A
+              db '+    ', 0x0D,0x0A
+              db ' ++++', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '+ + +', 0x0D,0x0A
+              db '+   +', 0x0D,0x0A
+              db '     ', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A
+              db '   + ', 0x0D,0x0A
+              db ' ++  ', 0x0D,0x0A
+              db '+++++', 0x0D,0x0A,0 
+
+    mov si, right_name
+    call print_at_position
+    jmp process_arrows
+
+move_down:
+    down_name db '+   +  +++    +    +++  +  +', 0x0D,0x0A
+              db '+ +    +    +   +   +   + ++', 0x0D,0x0A
+              db '++     ++   +   +   +   + ++', 0x0D,0x0A
+              db '+ +    +    +   +   +   ++ +', 0x0D,0x0A
+              db '+   +  +++  +   +  +++  +  +', 0x0D,0x0A,0
+
+    mov si, down_name  
+    call print_at_position   
+    jmp process_arrows
+
+print_at_position:
+    ; Mueve el cursor a la posición (x_pos, y_pos)
+    mov ah, 02h
+    mov bh, 00h           ; Página 0
+    mov dh, [y_pos1]      ; Fila (Y)
+    mov dl, [x_pos1]      ; Columna (X)
+    int 10h
+    
+    ; Mostrar el texto desde SI
+    call print_string;mov ah, 0x0E           ; Función para escribir caracteres
+
+    ret
+
+
+randomize_positions:
+    ; Generar posiciones aleatorias para los nombres
+    mov ah, 2Ch           ; Obtener el tiempo del sistema
+    int 1Ah               ; Esto nos da un "pseudo-random" valor
+
+    ; Usar el tiempo como semilla para x e y aleatorias
+    mov [x_pos1], dl      ; Posición X de 'Juan'
+    mov [y_pos1], dh      ; Posición Y de 'Juan'
+    mov [x_pos2], dl      ; Posición X de 'Pedro'
+    mov [y_pos2], dh      ; Posición Y de 'Pedro'
+
+    ret
+
+exit:
+    ; Detener ejecución
+    mov si, msg
+    call print_string
+    cli
+    hlt
+
+
+
 ; Datos de los mensajes
-welcome_msg db 'Bienvenido al juego "My name". Presiona Y para empezar.', 0
+welcome_msg db 'Bienvenido al juego "My name". Presiona y para empezar.', 0
 game_msg db 'Iniciando el juego...', 0
+msg db 'LLEGO', 0
+
+
+names db 'Kevin Sergio', 0
+x_pos1 db 10              ; Posición inicial X para 'Juan'
+y_pos1 db 10              ; Posición inicial Y para 'Juan'
+x_pos2 db 40              ; Posición inicial X para 'Pedro'
+y_pos2 db 15              ; Posición inicial Y para 'Pedro
+
 
 times 510-($-$$) db 0   ; Rellenar con ceros hasta 510 bytes
 dw 0xAA55               ; Firma de arranque
-
