1f8bc82df594eddaee3be95d925f8714487d2dac
Kevinfgn <kevin.guzman.navarro@gmail.com>
2024-09-10 21:28:07 -0600

    Cositas 2.0

--- boot/myName.asm
+++ boot/myName.asm
@@ -5,22 +5,27 @@ start:
 
     call set_console
     ; Muestra el menú principal
-    call show_main_menu
+    ;call show_main_menu
+    mov si, welcome_msg ; Dirección del mensaje
+    call print_string   ; Llama a la función para imprimir la cadena
 
 main_loop:
+
+    mov si, menu
+    call print_string
 
     ; Lee la tecla presionada
     mov ah, 0x00
     int 0x16                ; Interrupción para leer el teclado
 
     ; Ejecuta acciones basadas en la tecla presionada
-    cmp al, 'i'
+    cmp al, 'y'
     je start_application
 
     cmp al, 'r'
     je restart
 
-    cmp al, 'd'
+    cmp al, 'p'
     je exit_program
 
     ; Vuelve al menú principal si la tecla no es reconocida
@@ -33,7 +38,30 @@ start_application:
     call print_name
 
     ; Regresa al menú principal después de mostrar el mensaje
-    jmp main_loop
+    jmp process_arrows
+
+
+process_arrows:
+    mov ah, 00h           ; Leer el código extendido de la tecla
+    int 16h               ; Espera una tecla
+
+    ; Verificar si la tecla es W, A, S o D
+    cmp al, 'w'           ; Verificar si la tecla es 'w'
+    je kevin_up
+
+    cmp al, 'a'           ; Verificar si la tecla es 'a'
+    je kevin_left
+
+    cmp al, 's'           ; Verificar si la tecla es 's'
+    je kevun_down
+
+    cmp al, 'd'           ; Verificar si la tecla es 'd'
+    je kevin_right
+
+    jmp process_arrows    ; Continuar en el bucle de entrada de teclas
+
+
+
 
 print_name:
     mov bx, 0
@@ -103,63 +131,103 @@ exit_program:
     hlt
     jmp $
 
+
+print_string:
+    mov ah, 0x0E        ; Función 0Eh de BIOS para imprimir caracteres
+.next_char:
+    lodsb               ; Cargar el siguiente carácter de DS:SI en AL
+    cmp al, 0           ; Comprobar si es el final de la cadena
+    je .done            ; Si es cero, final de la cadena
+    int 0x10            ; Llamar a la interrupción de video
+    jmp .next_char      ; Continuar con el siguiente carácter
+.done:
+    ret
+
+
+
 menu db 'Presione alguna tecla:', 0x0D, 0x0A
      db '- i: iniciar', 0x0D, 0x0A
      db '- r: reiniciar', 0x0D, 0x0A
      db '- d: detener', 0x0D, 0x0A, 0
+kevin_up:
+    call set_console
+    KevinArriba db '+   +  +++  +   +  +++  +  +', 0x0D, 0x0A
+                db '+ +    +    +   +   +   ++ +', 0x0D, 0x0A
+                db '++     ++   +   +   +   + ++', 0x0D, 0x0A
+                db '+ +    +    +   +   +   + ++', 0x0D, 0x0A
+                db '+   +  +++    +    +++  +  +', 0x0D, 0x0A, 0
+    mov si ,KevinArriba
+    call print_string
+    jmp process_arrows
 
-KevinArriba db '+   +  +++  +   +  +++  +  +', 0x0D, 0x0A
-            db '+ +    +    +   +   +   ++ +', 0x0D, 0x0A
-            db '++     ++   +   +   +   + ++', 0x0D, 0x0A
-            db '+ +    +    +   +   +   + ++', 0x0D, 0x0A
-            db '+   +  +++    +    +++  +  +', 0x0D, 0x0A, 0
+kevin_left:
+    call set_console
+    KevinIzquie   db '+++++', 0x0D,0x0A
+                db '  ++ ', 0x0D,0x0A
+                db ' +   ', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '++++ ', 0x0D,0x0A
+                db '    +', 0x0D,0x0A
+                db '++++ ', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '+ + +', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db ' + + ', 0x0D,0x0A
+                db '  +  ', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A,0    
+    mov si ,KevinIzquie
+    call print_string
+    jmp process_arrows
 
-KevinIzquie   db '+++++', 0x0D,0x0A
-              db '  ++ ', 0x0D,0x0A
-              db ' +   ', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '++++ ', 0x0D,0x0A
-              db '    +', 0x0D,0x0A
-              db '++++ ', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '+ + +', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db ' + + ', 0x0D,0x0A
-              db '  +  ', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A,0    
+
+kevin_right:
+    call set_console
+    kevinDerecha  db '+++++', 0x0D,0x0A
+                db '  +  ', 0x0D,0x0A
+                db ' + + ', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A
+                db '+ + +', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db ' ++++', 0x0D,0x0A
+                db '+    ', 0x0D,0x0A
+                db ' ++++', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '+ + +', 0x0D,0x0A
+                db '+   +', 0x0D,0x0A
+                db '     ', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A
+                db '   + ', 0x0D,0x0A
+                db ' ++  ', 0x0D,0x0A
+                db '+++++', 0x0D,0x0A,0 
+    mov si ,kevinDerecha
+    call print_string
+    jmp process_arrows
 
 
-kevinDerecha  db '+++++', 0x0D,0x0A
-              db '  +  ', 0x0D,0x0A
-              db ' + + ', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A
-              db '+ + +', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db ' ++++', 0x0D,0x0A
-              db '+    ', 0x0D,0x0A
-              db ' ++++', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '+ + +', 0x0D,0x0A
-              db '+   +', 0x0D,0x0A
-              db '     ', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A
-              db '   + ', 0x0D,0x0A
-              db ' ++  ', 0x0D,0x0A
-              db '+++++', 0x0D,0x0A,0 
+kevun_down:
+    call set_console
+    kevinVuelta db '+   +  +++    +    +++  +  +', 0x0D,0x0A
+                db '+ +    +    +   +   +   + ++', 0x0D,0x0A
+                db '++     ++   +   +   +   + ++', 0x0D,0x0A
+                db '+ +    +    +   +   +   ++ +', 0x0D,0x0A
+                db '+   +  +++  +   +  +++  +  +', 0x0D,0x0A,0              
+    mov si ,kevinVuelta
+    call print_string
+    jmp process_arrows
 
 mensajeFin db 'Se ha terminado el programa...', 0
-
-times 732-($-$$) db 0   ; Rellenar con ceros hasta 510 bytes
+welcome_msg db 'Bienvenido al juego "My name". Presiona y para empezar.', 0
+times 980-($-$$) db 0   ; Rellenar con ceros hasta 510 bytes
 dw 0xAA55              ; Firma del sector de arranque
